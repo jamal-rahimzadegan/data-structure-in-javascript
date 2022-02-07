060e47f1a030370673e638f519b5d5beb45d3e3e
Jamal <jamalrahimzadegan@gmail.com>
2022-02-07 14:20:18 +0330

    Update stack.ts

--- stack/stack.ts
+++ stack/stack.ts
@@ -1,22 +1,23 @@
 export default class Stack {
-	maxSize: number;
 	protected items: any[];
+	maxSize: number;
 
 	constructor(maxSize = 10) {
 		this.maxSize = maxSize;
 		this.items = [];
 	}
 
-	push(element) {
+	push(newItem) {
 		if (this.isFull()) {
 			console.log("Stack Overflow!");
 			return;
 		}
 
-		this.items.push(element);
+		this.items.push(newItem);
 	}
 
 	peek() {
+		if (this.isEmpty()) return;
 		return this.items[this.items.length - 1];
 	}
 
@@ -33,11 +34,15 @@ export default class Stack {
 		console.log(this.items);
 	}
 
-	isEmpty() {
+	getLength(): number {
+		return this.items.length;
+	}
+
+	isEmpty(): boolean {
 		return this.items.length === 0;
 	}
 
-	isFull() {
-		return this.items.length >= this.maxSize;
+	isFull(): boolean {
+		return this.items.length === this.maxSize;
 	}
 }
